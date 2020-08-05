b5763ea320d49b7e5b2100e7163ecd8e4e1411d7
Eloy <eloydgutierrez@gmail.com>
2020-08-05 15:17:43 -0400

    MVP for Intro-Python-1 met

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,9 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(number):
+    if number % 2 == 0:
+        return True
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -9,4 +12,9 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+result = is_even(num)
 
+if result == True:
+    print("Even!")
+else:
+    print("Odd")
